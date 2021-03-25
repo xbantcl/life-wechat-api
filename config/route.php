5943da19e60d2818b0d24d6031bee5dc09d5007f
steven <steven@gmail.com>
2021-03-25 20:19:05 +0800

    update

--- config/route.php
+++ config/route.php
@@ -7,16 +7,24 @@ $app->group('/api/', function (RouteCollectorProxy $group) {
     $group->post('file/delete', 'Dolphin\Ting\Http\Service\FileService:delete');
 
     // ------------------ 圈子动态接口 -------------------------------
-    $group->post('circle/add', 'Dolphin\Ting\Http\Service\CircleService:add');
     $group->post('circle/list', 'Dolphin\Ting\Http\Service\CircleService:getList');
+
+    // ------------------ 车位相关接口 -------------------------------
+    $group->post('carplace/list', 'Dolphin\Ting\Http\Service\CarPlaceService:getList');
+    $group->post('carplace/detail', 'Dolphin\Ting\Http\Service\CarPlaceService:detail');
+    $group->post('carplace/comment/list', 'Dolphin\Ting\Http\Service\CarPlaceService:commentList');
+});
+
+$app->group('/api/', function (RouteCollectorProxy $group) {
+    // ------------------ 文件接口 ----------------------------------
+
+    // ------------------ 圈子动态接口 -------------------------------
+    $group->post('circle/add', 'Dolphin\Ting\Http\Service\CircleService:add');
     $group->post('circle/comment', 'Dolphin\Ting\Http\Service\CircleService:comment');
 
     // ------------------ 车位相关接口 -------------------------------
     $group->post('carplace/add', 'Dolphin\Ting\Http\Service\CarPlaceService:add');
-    $group->post('carplace/list', 'Dolphin\Ting\Http\Service\CarPlaceService:getList');
     $group->post('carplace/comment', 'Dolphin\Ting\Http\Service\CarPlaceService:comment');
-    $group->post('carplace/detail', 'Dolphin\Ting\Http\Service\CarPlaceService:detail');
-    $group->post('carplace/comment/list', 'Dolphin\Ting\Http\Service\CarPlaceService:commentList');
     $group->post('carplace/comment/delete', 'Dolphin\Ting\Http\Service\CarPlaceService:deleteComment');
 })->addMiddleware(new \Dolphin\Ting\Bootstrap\Middleware\AuthMiddleware($container));
 
