43e0f08190fe3fb0390e822092017845fd447004
steven <steven@gmail.com>
2021-09-10 15:49:50 +0800

    update

--- app/Http/Constant/RecycleConstant.php
+++ app/Http/Constant/RecycleConstant.php
@@ -9,9 +9,20 @@ class RecycleConstant
     const FINISHED = 3;    // 完成
     const CANCEL = 4; // 取消
     const PRICES = [
-        'paper' => 0.5,
+        'paper' => [
+            'tz'  => 0.5,
+            'hz'  => 0.5,
+            'chz' => 0.5,
+            'zz'  => 0.5,
+            'bz'  => 0.5
+        ],
         'plastic' => 1.0,
-        'metal' => 2.0,
+        'metal' => [
+            'tie' => 2.0,
+            'tong' => 10.0,
+            'lv'   => 3.0,
+            'ylg'  => 4.0
+        ],
         'dress' => 0.7,
         'hde' => 0.0
     ];
