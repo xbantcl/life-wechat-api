b895553f1ab14f2c4f9b0d5cef3fd7d5b365dcf7
steven <steven@gmail.com>
2021-10-29 17:56:00 +0800

    update

--- config/route.php
+++ config/route.php
@@ -25,7 +25,6 @@ $app->group('/api/', function (RouteCollectorProxy $group) {
     // ------------------ 二手商品相关接口 -------------------------------
     $group->post('secondhand/detail', 'Dolphin\Ting\Http\Service\SecondhandGoodsService:detail');
     $group->post('secondhand/list', 'Dolphin\Ting\Http\Service\SecondhandGoodsService:getList');
-    $group->post('secondhand/user/list', 'Dolphin\Ting\Http\Service\SecondhandGoodsService:getListByUid');
 
     // ------------------ 拼车相关接口 -----------------------------------
     $group->post('pinche/list', 'Dolphin\Ting\Http\Service\PincheService:getList');
